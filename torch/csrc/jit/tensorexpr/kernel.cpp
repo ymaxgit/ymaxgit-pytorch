18953a1aa3079f60303c43a3091db73dd9b049ac
Hui Guo <huiguo@fb.com>
2021-10-23 05:09:16 -0700

    [tensorexpr] Memory planning: add pre-allocation for reused intermediate buffers
    ghstack-source-id: 5036ac6b13f75ed8ea40527e4dfd4ab11f06c898
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/66967

--- torch/csrc/jit/tensorexpr/kernel.cpp
+++ torch/csrc/jit/tensorexpr/kernel.cpp
@@ -725,13 +725,7 @@ StmtPtr TensorExprKernel::transformLoops(BackendType backendType, StmtPtr st) {
     }
   }
 
-  if (pre_alloc_) {
-    auto interm_bufs = l.getIntermediateBufs();
-    preAllocIntermediateBufs(interm_bufs);
-    l.prepareForCodegen();
-  } else {
-    l.prepareForCodegen();
-  }
+  l.prepareForCodegen();
 
   GRAPH_DEBUG("after prepareForCodegen", *l.root_stmt());
   l.simplify();
@@ -1045,9 +1039,8 @@ void TensorExprKernel::bindConstant(const torch::jit::Value* v) {
 }
 
 void TensorExprKernel::preAllocIntermediateBufs(
-    std::unordered_set<BufPtr>& interm_bufs) {
-  std::vector<std::pair<BufPtr, void*>> allocated_bufs;
-  for (auto it = interm_bufs.begin(); it != interm_bufs.end();) {
+    std::vector<BufPtr>& interm_bufs) {
+  for (auto it = interm_bufs.begin(); it != interm_bufs.end(); ++it) {
     // Check if buf shape is static and compute its size if static.
     auto buf = *it;
     bool is_static = true;
@@ -1062,25 +1055,13 @@ void TensorExprKernel::preAllocIntermediateBufs(
     }
     // Only allocate memory for static bufs.
     if (!is_static) {
-      ++it;
       continue;
     }
     auto bp = (void*)malloc(size);
     if (!bp) {
-      ++it;
       continue;
     }
-    allocated_bufs.emplace_back(buf, bp);
-    it = interm_bufs.erase(it);
-  }
-  std::sort(
-      allocated_bufs.begin(),
-      allocated_bufs.end(),
-      [](const auto& a, const auto& b) {
-        return a.first->name_hint() > b.first->name_hint();
-      });
-  for (auto& a : allocated_bufs) {
-    constants_.push_back({a.first, a.second});
+    constants_.push_back({buf, bp});
   }
 }
 
@@ -1172,7 +1153,13 @@ void TensorExprKernel::compile() {
       stmt,
       bufferArgs_,
       device_,
-      SubgraphUtils::generateNameForGraph(graph_));
+      SubgraphUtils::generateNameForGraph(graph_),
+      pre_alloc_);
+
+  if (pre_alloc_) {
+    auto interm_bufs = codegen_->getIntermediateBufs();
+    preAllocIntermediateBufs(interm_bufs);
+  }
 }
 
 TensorExprKernel::TensorExprKernel(
